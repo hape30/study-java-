b01c6d5256364a46b5e9ffc191c4c55f30f47761
hape30 <sasha2007.ivanov@mail.ru>
2024-06-08 03:02:09 +0300

    first start

---
+++ Authenticator.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class Authenticator {
+    public static void main(String[] args) {
+        
+        String username = 
+                JOptionPane.showInputDialog("Name user ");
+        String password = 
+                JOptionPane.showInputDialog("Password");
+        
+        if(
+            username != null && password != null &&
+            (
+                (username.equals("burd") &&
+                password.equals("swordfish")) ||
+                (username.equals("hritter") &&
+                password.equals("preakston"))
+
+            )
+        )
+        {
+            JOptionPane.showMessageDialog(null, "Welcome");
+        } else {
+            JOptionPane.showMessageDialog(null, "Password is incorrect"); 
+        }
+        }
+        
+    }
---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner prostata = new Scanner(System.in);
+        System.out.println("Size u prostata?");
+        String size = prostata.nextLine();
+        System.out.println("u zise \s " +  size);
+        
+
+
+        
+    }
+}
